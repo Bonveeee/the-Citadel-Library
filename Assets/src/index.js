2d1e5fdbcfdd63de66443df98404be49f6392291
bonveeee <bonochieng@gmail.com>
2022-06-23 14:44:32 +0300

    started fetching data from API

--- Assets/src/index.js
+++ Assets/src/index.js
@@ -1,4 +1,6 @@
-
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+});
 // Header -->
 // Nav -->
 
@@ -9,6 +11,26 @@
 // End Landing Section -->
 
 // Books Collection Section -->
+const h1 = document.getElementById("titlebooksection");
+h1.textContent = "BOOKS IN STORE";
+//document.body.appendChild('h1');   //has a bug
+
+const url = "https://gutendex.com/books/";
+function parseResponse(response) {
+  return response.json();
+}
+function getAllBooks() {
+  fetch(url)
+    .then(parseResponse)
+    .then((data) => {
+      data.forEach((results) => {
+        let img = document.getElementById("coverimage");
+        console.log(img);
+        img.src = results[0].formats["image/jpeg"];
+      });
+    });
+}
+getAllBooks();
 
 //  Books Collection End Section -->
 
